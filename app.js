2f2efa3a136d42d2fa7698458297098364675357
Muhammad-Sahil <86284205+Muhammad-Sahil@users.noreply.github.com>
2021-07-16 16:26:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// var a = document.getElementById("value1")
+// function cal(num){
+//   a.value += num
+// }
+
+// function result(){
+//     a.value = eval(a.value)
+// }
+// function reset(){
+//     a.value = ""
+    
+// }
+
